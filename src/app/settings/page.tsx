130319371ed164130b7231afba7a290d73654e08
francehk <francehk23@gmail.com>
2026-09-13 10:08:28 +0300

    feat: add settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { requireUser } from "@/lib/auth";
+import { AppShell } from "@/components/layout/app-shell";
+import { SettingsView } from "@/components/settings/settings-view";
+
+export const metadata: Metadata = { title: "Mipangilio" };
+
+export default async function SettingsPage() {
+  const user = await requireUser();
+
+  return (
+    <AppShell user={{ jina: user.jina, jinaDuka: user.jina_duka }}>
+      <SettingsView
+        user={{
+          jina: user.jina,
+          jinaDuka: user.jina_duka,
+          simu: user.simu,
+        }}
+        smsConfigured={Boolean(process.env.BEEM_API_KEY && process.env.BEEM_API_SECRET)}
+      />
+    </AppShell>
+  );
+}
